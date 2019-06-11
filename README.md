eab92d27c1d4462a27ff6e2330dfb995d18822d4
Jan Henckens <jan@studioespresso.co>
2019-06-11 18:03:43 +0200

    Base plugin

---
+++ README.md
@@ -0,0 +1,26 @@
+# Mollie Payments plugin for Craft CMS 3.x
+
+Easily accept payment with Mollie Payments
+
+## Requirements
+
+This plugin requires Craft CMS 3.1.0 or later.
+
+## Installation
+
+To install the plugin, follow these instructions.
+
+1. Open your terminal and go to your Craft project:
+
+        cd /path/to/project
+
+2. Then tell Composer to load the plugin:
+
+        composer require studioespresso/craft-mollie-payments
+
+3. In the Control Panel, go to Settings → Plugins and click the “Install” button for Mollie Payments.
+
+
+* Release it
+
+Brought to you by [Studio Espresso](https://studioespresso.co)
---
+++ src/MolliePayments.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Mollie Payments plugin for Craft CMS 3.x
+ *
+ * Easily accept payment with Mollie Payments
+ *
+ * @link      https://studioespresso.co
+ * @copyright Copyright (c) 2019 Studio Espresso
+ */
+
+namespace studioespresso\molliepayments;
+
+use studioespresso\molliepayments\variables\MolliePaymentsVariable;
+use studioespresso\molliepayments\twigextensions\MolliePaymentsTwigExtension;
+use studioespresso\molliepayments\models\Settings;
+
+use Craft;
+use craft\base\Plugin;
+use craft\services\Plugins;
+use craft\events\PluginEvent;
+use craft\web\UrlManager;
+use craft\services\Elements;
+use craft\web\twig\variables\CraftVariable;
+use craft\events\RegisterComponentTypesEvent;
+use craft\events\RegisterUrlRulesEvent;
+
+use yii\base\Event;
+
+/**
+ * Class MolliePayments
+ *
+ * @author    Studio Espresso
+ * @package   MolliePayments
+ * @since     1.0.0
+ *
+ */
+class MolliePayments extends Plugin
+{
+    // Static Properties
+    // =========================================================================
+
+    /**
+     * @var MolliePayments
+     */
+    public static $plugin;
+
+    // Public Properties
+    // =========================================================================
+
+    /**
+     * @var string
+     */
+    public $schemaVersion = '1.0.0';
+
+    // Public Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+        self::$plugin = $this;
+
+        Craft::$app->view->registerTwigExtension(new MolliePaymentsTwigExtension());
+
+        Event::on(
+            UrlManager::class,
+            UrlManager::EVENT_REGISTER_CP_URL_RULES,
+            function (RegisterUrlRulesEvent $event) {
+                $event->rules['cpActionTrigger1'] = 'mollie-payments/default/do-something';
+            }
+        );
+
+        Event::on(
+            Elements::class,
+            Elements::EVENT_REGISTER_ELEMENT_TYPES,
+            function (RegisterComponentTypesEvent $event) {
+
+            }
+        );
+
+        Event::on(
+            CraftVariable::class,
+            CraftVariable::EVENT_INIT,
+            function (Event $event) {
+                /** @var CraftVariable $variable */
+                $variable = $event->sender;
+                $variable->set('molliePayments', MolliePaymentsVariable::class);
+            }
+        );
+
+    }
+
+    // Protected Methods
+    // =========================================================================
+
+    /**
+     * @inheritdoc
+     */
+    protected function createSettingsModel()
+    {
+        return new Settings();
+    }
+}
