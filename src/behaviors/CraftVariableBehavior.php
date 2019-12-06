9fe70d4119793a5b5942643fe39b1b91ba71228c
Jan Henckens <jan@studioespresso.co>
2019-12-06 20:38:09 +0100

    Added payments twig query

---
+++ src/behaviors/CraftVariableBehavior.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace studioespresso\molliepayments\behaviors;
+
+use craft\elements\db\EntryQuery;
+use studioespresso\molliepayments\elements\db\PaymentQuery;
+use studioespresso\molliepayments\elements\Payment;
+use yii\base\Behavior;
+
+/**
+ * Class EntryQueryBehavior
+ *
+ * @property EntryQuery $owner
+ */
+class CraftVariableBehavior extends Behavior
+{
+
+    public function payments($criteria = null): PaymentQuery
+    {
+        $query = Payment::find();
+        if ($criteria) {
+            Craft::configure($query, $criteria);
+        }
+        return $query;
+    }
+}
