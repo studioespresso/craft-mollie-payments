e413a8ac34f37304be7ccf8f702c06d9a95156c5
Jan Henckens <jan@statik.be>
2023-01-16 20:32:20 +0100

    Vuepress > Vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,38 @@
+module.exports = {
+    title: 'Mollie Payments for Craft',
+    base: '/craft-mollie-payment',
+    themeConfig: {
+
+        // logo: {light: '/icon-vuepress.svg', dark: '/icon-vuepress-light.svg'},
+        sidebar: [
+            {
+                items: [
+                    {text: 'Introduction', link: '/index'},
+                    {text: 'Getting started', link: '/general'},
+                    {text: 'Templating', link: '/template'},
+                    {text: 'Events', link: '/events'},
+                    {text: 'Webhook', link: '/webhook'},
+                    {text: 'Element', link: '/element'},
+                    {text: 'Settings', link: '/settings'},
+                ]
+            },
+
+
+        ],
+        nav: [
+            {
+                text: 'Buy now',
+                link: 'https://plugins.craftcms.com/mollie-payments',
+            },
+            {
+                text: 'Report an issue',
+                link: 'https://github.com/studioespresso/craft-mollie-payments/issues'
+            },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/studioespresso/craft-mollie-payments'
+            }
+        ]
+
+    }
+};
---
+++ docs/index.md
@@ -0,0 +1,21 @@
+---
+title: Mollie Payments
+---
+# Mollie Payments for Craft CMS
+
+Mollie Payments gives developers an easy way to accept payments on your website.
+
+The plugin doesn't handle products, taxes or price calculation, giving you the freedom to use it in any way you need to.
+
+A one-off payment for a donation, event registration with online payments or an all out javascript cart implementation, stick this plugin to the end of that flow and accept payments in 1, 2 ,3.
+
+*Note that this plugin is not built, affilated or supported by Mollie and that this plugin does not work with Craft Commerce*
+
+__If you are looking for a plugin to use Mollie with Craft Commerce, click [here](https://plugins.craftcms.com/commerce-mollie)__
+
+### Quick links
+- [Getting started](/general.html)
+- [Report an issue](https://github.com/studioespresso/craft-mollie-payments/issues/new)
+
+ 
+
