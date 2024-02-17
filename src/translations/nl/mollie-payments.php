a80c55ea1e53dbb5239f82c8892437409294aeb8
Jan Henckens <jan@henckens.be>
2024-02-17 17:24:43 +0100

    And even more translations

--- src/translations/nl/mollie-payments.php
+++ src/translations/nl/mollie-payments.php
@@ -1,19 +1,32 @@
 <?php
 
 return [
+    'Payment' => 'Betaling',
+    'payment' => 'betaling',
     'Payments' => 'Betalingen',
+    'payments' => 'betalingen',
     'Settings' => 'Instellingen',
     'Forms' => 'Formulieren',
+    'Form' => 'Formulier',
     'No forms exist yet.' => 'Geen formulieren gevonden',
     'Create new form' => 'Nieuw formulier maken',
     'New form' => 'Nieuw formulier',
     'Are you sure you want to delete “{name}”?' => 'Formulier “{name}” verwijderen?',
     '{name} deleted.' => '{name} verwijderd.',
-    'payment' => 'betaling',
-    'payments' => 'betalingen',
     'Amount' => 'Bedrag',
     'Date Created' => 'Datum aangemaakt',
     'Currency' => 'Munteenheid',
     'Payment description' => 'Beschrijving van de betaling',
     'How these payments are called in Mollie. You can include tags that output entry properties, such as {myCustomField}. Defaults to "Order #payment.id".' => 'Referenties van de betaling bij Mollie. Je kan velden van de betaling gebruiken, op deze manier: {myCustomField}. Standaard waarde is "Order #payment.id".',
+    'In Cart' => 'In mandje',
+    'Pending' => 'In afwachting',
+    'Free' => 'Gratis',
+    'Paid' => 'Betaald',
+    'Expired' => 'Vervallen',
+    'Refunded' => 'Terugbetaald',
+    'Transactions' => 'Transacties',
+    'Date' => 'Datum',
+    'Method' => 'Betalingsmethode',
+    'Payment for ' => 'Betaling voor '
 ];
+
