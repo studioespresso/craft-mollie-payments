de09807f5c6f3ccd4349c160ca2f40cccb48b428
Jan Henckens <jan@henckens.be>
2024-06-29 20:49:22 +0200

    Use crafts status ui for transactions

--- src/records/PaymentTransactionRecord.php
+++ src/records/PaymentTransactionRecord.php
@@ -3,6 +3,9 @@
 namespace studioespresso\molliepayments\records;
 
 use craft\db\ActiveRecord;
+use craft\enums\Color;
+use craft\helpers\Cp;
+use Mollie\Api\Types\PaymentStatus;
 
 class PaymentTransactionRecord extends ActiveRecord
 {
@@ -10,9 +13,27 @@ class PaymentTransactionRecord extends ActiveRecord
     {
         return '{{%mollie_transactions}}';
     }
-    
+
     public static function primaryKey()
     {
         return ['id'];
+    }
+
+    public function getStatusComponent()
+    {
+
+        $color = match ($this->status) {
+            PaymentStatus::STATUS_OPEN, PaymentStatus::STATUS_PENDING, => Color::Gray,
+            PaymentStatus::STATUS_PAID, PaymentStatus::STATUS_AUTHORIZED, 'free' => Color::Green,
+            PaymentStatus::STATUS_EXPIRED, PaymentStatus::STATUS_CANCELED, PaymentStatus::STATUS_FAILED => Color::Red,
+            default => Color::Gray,
+        };
+
+        return Cp::statusLabelHtml([
+            'color' => $color->value,
+            'icon' => null,
+            'label' => $this->status,
+            'indicatorClass' => null
+        ]);
     }
 }
